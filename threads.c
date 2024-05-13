24a3a4cb1c800d8d270bc1d83af8379d625634d5
Ekaterina Mikaylova <ekaterinamikaylova@Ekaterinas-Air.home>
2024-05-13 03:03:24 +0200

    forks fixed

--- threads.c
+++ threads.c
@@ -29,6 +29,6 @@ void *routine(void *arg)
     t_philo *philo;
 
     philo = (t_philo *)arg;
-    printf("Hello from philo %d, my left fork is %d and right forkis %d\n", philo->philo_id, *((int *)philo->l_fork), *((int *)philo->r_fork));
+    printf("Hello from philo %d, my left fork is %p and right forkis %p\n", philo->philo_id, philo->l_fork, philo->r_fork);
     return NULL;
 }
